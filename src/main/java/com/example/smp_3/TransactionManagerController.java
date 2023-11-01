5db0fc1a33f41bf729b7b78629f6707079a1b119
ytfarkas <ytfarkas@gmail.com>
2023-11-01 04:08:50 -0400

    broccoli

--- src/main/java/com/example/smp_3/TransactionManagerController.java
+++ src/main/java/com/example/smp_3/TransactionManagerController.java
@@ -1,57 +1,88 @@
 package com.example.smp_3;
 
+import javafx.beans.binding.BooleanBinding;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.control.CheckBox;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 
 public class TransactionManagerController {
-        @FXML
-        private ToggleButton closeButton;
 
-        @FXML
-        private ToggleButton openButton;
+    ObservableList<String> accountTypeList = FXCollections.observableArrayList(
+            "Checking", "College Checking", "Savings", "Money Market");
 
-        @FXML
-        private CheckBox isLoyal;
+    @FXML
+    private ToggleButton closeButton;
 
-        @FXML
-        private ToggleGroup open_close;
+    @FXML
+    private ToggleButton openButton;
 
-        @FXML
-        private ComboBox<String> accountType;
+    @FXML
+    private CheckBox isLoyal;
 
+    @FXML
+    private ToggleGroup open_close;
 
-        //public void initialize(URL url, ResourceBundle resourceBundle){
-          //  accountType.setItems(FXCollections.observableArrayList("Checking", "College Checking", "Savings", "Money Market"));
-        //}
+    @FXML ToggleGroup campusLocation;
 
-        @FXML
-        public void initilize(){
-            accountType.getItems().removeAll((accountType.getItems()));
-            accountType.getItems().addAll("Checking", "College Checking", "Savings", "Money Market");
+    @FXML
+    private ComboBox accountType;
+    @FXML
+    private TextField firstName;
 
-        }
+    @FXML
+    private TextField lastName;
 
+    @FXML
+    private DatePicker dateOfBirth;
 
-        @FXML
-        void openCloseToggleButton(ActionEvent event) {
-            if (event.getSource() == openButton){
-                isLoyal.setVisible(true);
-            }
-           if (event.getSource() == closeButton){
-                isLoyal.setVisible(false);
-            }
+    @FXML
+    private GridPane campusContainer;
 
-
-        }
+    @FXML
+    private void initialize(){
+        BooleanBinding noneSelected = openButton.selectedProperty().not().and(closeButton.selectedProperty().not());
+        firstName.disableProperty().bind(noneSelected);
+        lastName.disableProperty().bind(noneSelected);
+        dateOfBirth.disableProperty().bind(noneSelected);
+        accountType.disableProperty().bind(noneSelected);
+        isLoyal.setVisible(false);
+        campusContainer.setVisible(false);
+        accountType.setItems(accountTypeList);
 
     }
+
+
+
+    @FXML
+    void openCloseToggleButton(ActionEvent event) {
+        if (event.getSource() == openButton){
+
+        }
+        else {
+            isLoyal.setVisible(false);
+        }
+    }
+    @FXML
+    void checkAccount (ActionEvent event){
+        if (accountType.getSelectionModel().getSelectedItem().equals("Savings")
+                ||accountType.getSelectionModel().getSelectedItem().equals("Money Market")) {
+            isLoyal.setVisible(true);
+        }
+        else{
+            isLoyal.setVisible(false);
+        }
+        if (accountType.getSelectionModel().getSelectedItem().equals("College Checking")){
+            campusContainer.setVisible(true);
+        }
+        else {
+            campusContainer.setVisible(false);
+        }
+    }
+
+}
 
